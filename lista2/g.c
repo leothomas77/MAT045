23184937a2de95a7c5b2664474a32130165c9e2d
leothomas77 <leothomas77@yahoo.com.br>
2018-05-07 23:11:11 -0300

    Update g.c

--- lista2/g.c
+++ lista2/g.c
@@ -1,4 +1,3 @@
-
 //Lista 2 Questao G
 #include <stdio.h>
 
@@ -7,14 +6,23 @@ int main()
     int n, s, movimentacao;
     scanf("%d %d", &n, &s);
     
+    if (n < -1 || n > 30) {
+        return 0;
+    } 
+    
+    if (s < -1000 || s > 1000) {
+        return 0;
+    }
+    
     int i = 0;
     while (i < n) {
         scanf("%d", &movimentacao);
-        s += movimentacao;
+        if (movimentacao >= -1000 && movimentacao <= 1000) {
+            s += movimentacao;
+        }
         i++;
     }
     printf("%d", s);
     
     return 0;
 }
-
