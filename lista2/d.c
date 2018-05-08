26baaeef67fd38c6909e4a28700bf69b22d03fd6
leothomas77 <leothomas77@yahoo.com.br>
2018-05-07 22:05:36 -0300

    Update d.c

--- lista2/d.c
+++ lista2/d.c
@@ -1,42 +1,43 @@
-//Lista 2 Questao B
+//Lista 2 Questao D - álbum de fotos
 #include <stdio.h>
-#define RETRATO 0
-#define PAISAGEM 1
 int main()
 {
-    int x, y, l, h;
-    int orientacaoPagina, orientacaoFoto;
+    int x, y, l1, h1, l2, h2;
+    bool posicao1, posicao2, posicao3, posicao4;
+    
+    scanf("%d %d", &x, &y); // dimensoes da pagina
+    
+    scanf("%d %d", &l1, &h1); // dimensoes da foto 1
+    scanf("%d %d", &l2, &h2); // dimensoes da foto 2
+    
+    posicao1 = posicao2 = posicao3 = posicao4 = false;
+    
+    //posicao 1
+    if ((l1 + l2) <= x && h1 <= y && h2 <= y) {
+        posicao1 = true;
+    }
+    
+    //posicao 2
+    if ((l1 + h2) <= x && h1 <= y && l2 <= y) {
+        posicao2 = true;
+    }
+    
+    //posicao 3
+    if ((h1 + l2) <= x && l1 <= y && h2 <= y) {
+        posicao3 = true;
+    }
+    
+    //posicao 4
+    if ((h1 + h2) <= x && l1 <= y && l2 <= y) {
+        posicao4 = true;
+    }
     
     
-    scanf("%d %d", &x, &y);
-    if (y > x) {
-        orientacaoPagina = RETRATO
+    if (posicao1 || posicao2 || posicao3 || posicao4) {
+        printf("S\n");
     } else {
-        orientacaoPagina = PAISAGEM;
+        printf("N\n");
     }
 
-    for (int i = 0; i < 2; i++) {
-        scanf("%d %d", &l, &h);
-        
-        if (l > h) {
-            orientacaoFoto = RETRATO
-        } else {
-            orientacaoFoto = PAISAGEM;
-        }
-        
-        if (orientacaoPagina == RETRATO) {
-                    
-        } else {
-            
-        }
-
-    }
-
-    if (x >= 0 && y >= 0) {
-        printf("S %d %d\n", x, y);
-    } else {
-        printf("N %d %d\n", x, y);
-    }
-    
     return 0;
 }
