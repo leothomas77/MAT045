0d0f9ab87b0179ac89a72ce06862cac59ff7b512
leothomas77 <leothomas77@yahoo.com.br>
2018-05-08 01:18:59 -0300

    Update d.c

--- lista2/d.c
+++ lista2/d.c
@@ -1,43 +1,49 @@
-//Lista 2 Questao D - álbum de fotos
+//Lista 2 Questao D - Album de fotos
 #include <stdio.h>
-int main()
+int
+main ()
 {
-    int x, y, l1, h1, l2, h2;
-    int posicao1, posicao2, posicao3, posicao4;
-    
-    scanf("%d %d", &x, &y); // dimensoes da pagina
-    
-    scanf("%d %d", &l1, &h1); // dimensoes da foto 1
-    scanf("%d %d", &l2, &h2); // dimensoes da foto 2
-    
-    posicao1 = posicao2 = posicao3 = posicao4 = 0;
-    
-    //posicao 1
-    if ((l1 + l2) <= x && h1 <= y && h2 <= y) {
-        posicao1 = 1;
-    }
-    
-    //posicao 2
-    if ((l1 + h2) <= x && h1 <= y && l2 <= y) {
-        posicao2 = 1;
-    }
-    
-    //posicao 3
-    if ((h1 + l2) <= x && l1 <= y && h2 <= y) {
-        posicao3 = 1;
-    }
-    
-    //posicao 4
-    if ((h1 + h2) <= x && l1 <= y && l2 <= y) {
-        posicao4 = 1;
-    }
-    
-    
-    if (posicao1 || posicao2 || posicao3 || posicao4) {
-        printf("S\n");
-    } else {
-        printf("N\n");
+  int x, y, l1, h1, l2, h2;
+  int colou_fotos = 0;;
+
+  scanf ("%d %d", &x, &y);	// dimensoes da pagina
+
+  scanf ("%d %d", &l1, &h1);	// dimensoes da foto 1
+  scanf ("%d %d", &l2, &h2);	// dimensoes da foto 2
+
+  //posicao 1
+  if ((l1 + l2) <= x && h1 <= y && h2 <= y)
+    {
+      colou_fotos++;
     }
 
-    return 0;
+  //posicao 2
+  if ((l1 + h2) <= x && h1 <= y && l2 <= y)
+    {
+      colou_fotos++;
+    }
+
+  //posicao 3
+  if ((h1 + l2) <= x && l1 <= y && h2 <= y)
+    {
+      colou_fotos++;
+    }
+
+  //posicao 4
+  if ((h1 + h2) <= x && l1 <= y && l2 <= y)
+    {
+      colou_fotos++;
+    }
+
+
+  if (colou_fotos)
+    {
+      printf ("S\n");
+    }
+  else
+    {
+      printf ("N\n");
+    }
+
+  return 0;
 }
