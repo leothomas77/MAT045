dad1169a7bcbeaa6942c0eada9fd7d0da1d53ed4
leothomas77 <leothomas77@yahoo.com.br>
2018-06-13 14:27:02 -0300

    Update d.c

--- lista3/d.c
+++ lista3/d.c
@@ -5,15 +5,14 @@ int main() {
     int A, V, X, Y, voos_aeroportos[MAX], index = 1;
     int maiorFrequencia = 0;
     for( ; ; ) {
-        for (int i = 0; i < MAX; i++) {
-            voos_aeroportos[i] = 0;
-        }   
-
         scanf("%d%d", &A, &V);
         if (A == 0  && V == 0) {
             break;
         }
-        
+        for (int i = 0; i < MAX; i++) {
+            voos_aeroportos[i] = 0;
+        }   
+       
         for(int i = 0; i < V; i++) {
             scanf("%d%d", &X, &Y);
             voos_aeroportos[X]++;
@@ -31,7 +30,7 @@ int main() {
         for(int i = 1; i <= A; i++) {
             if (voos_aeroportos[i] == maiorFrequencia) {
                 if (cont == 0) {
-                    printf("%d ", i);
+                    printf("%d", i);
                 } else {
                     printf(" %d", i);
                 }
