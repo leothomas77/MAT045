15dd97f8c21c1f59d0bfe232b35000aa2fdc689c
leothomas77 <leothomas77@yahoo.com.br>
2018-06-12 23:03:15 -0300

    Create e.c

---
+++ lista3/e.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAX 100
+
+int main() {
+    int N, territorio, territorios[MAX], somaMetade = 0, somaTotal = 0;
+    for (int i = 0; i < MAX; i++) {
+        territorios[i] = 0;
+    }
+
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &territorio);
+        territorios[i] = territorio;
+        somaTotal = somaTotal + territorios[i];
+    }
+    
+    int i;
+    for(i = 0; i < N; i++) {
+        somaMetade = somaMetade + territorios[i];
+        if (somaMetade == somaTotal / 2) {
+            break;
+        }
+    }
+    
+    printf("%d", i + 1);
+
+    return 0;
+}
