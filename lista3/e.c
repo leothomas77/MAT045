139a1604bdd08f937c7992ee46f198e8d20d3c60
leothomas77 <leothomas77@yahoo.com.br>
2018-06-13 00:25:52 -0300

    Update e.c

--- lista3/e.c
+++ lista3/e.c
@@ -2,7 +2,7 @@
 #define MAX 100
 
 int main() {
-    int N, territorio, territorios[MAX], somaMetade = 0, somaTotal = 0;
+    int N = 1, territorio, territorios[MAX], somaMetade = 0, somaTotal = 0;
     for (int i = 0; i < MAX; i++) {
         territorios[i] = 0;
     }
@@ -15,7 +15,7 @@ int main() {
         somaTotal = somaTotal + territorios[i];
     }
     
-    int i;
+    int i = 0;
     for(i = 0; i < N; i++) {
         somaMetade = somaMetade + territorios[i];
         if (somaMetade == somaTotal / 2) {
